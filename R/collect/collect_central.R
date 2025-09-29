b2f5d4f24383fd311cd49521a6fbe8b568a851bd
tarssioesa <tarssio.disap@hotmail.com>
2025-09-29 10:26:57 -0300

    tarssio update collect

---
+++ R/collect/collect_central.R
@@ -0,0 +1,9 @@
+### Load: 
+require(dplyr)
+
+
+#### Chamando os dados: 
+
+source("R/collect/collect_data_sistemas.R")
+source("R/collect/collect_transferencias.R")
+source("R/collect/vazao_transferencia.R")
